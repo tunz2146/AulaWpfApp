0a4737e01bdea2369c541a90c0eb1a8c55a39c6f
Tunz <khanhquynhcute1528@gmail.com>
2026-09-10 05:14:56 +0700

    Initial commit

---
+++ AulaWpfApp/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace AulaWpfApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ScanKeyboard_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(
+                "Đang quét bàn phím AULA 60 HE...",
+                "AULA Control",
+                MessageBoxButton.OK,
+                MessageBoxImage.Information
+            );
+        }
+    }
+}
